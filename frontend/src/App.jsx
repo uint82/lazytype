f26b01e6ea47ea245e270028d55e4494643f7bee
uint82 <abrorhilmi6@gmail.com>
2025-11-15 09:38:35 +0700

    add: Vercel analytic package for visitor tracking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Lazytype from "./pages/Lazytype";
 import Footer from "./components/Footer";
@@ -31,6 +32,7 @@ const App = () => {
       </main>
 
       <Footer isTyping={!showConfig && !isTestComplete} />
+      <Analytics />
     </div>
   );
 };
