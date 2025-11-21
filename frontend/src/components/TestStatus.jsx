3a56085ae9496de3528d59e4736bb38bdf7c0610
uint82 <abrorhilmi6@gmail.com>
2025-11-21 21:55:53 +0700

    add: new status for custom mode

--- frontend/src/components/TestStatus.jsx
+++ frontend/src/components/TestStatus.jsx
@@ -1,33 +1,61 @@
 const TestStatus = ({
   selectedMode,
   selectedDuration,
-  totalWords,
+  selectedWordCount,
   wordsTyped,
   timeElapsed,
+  totalQuoteWords,
 }) => {
   const formatTime = (seconds) => {
     if (seconds < 60) {
       return `${seconds}`;
     }
-    const mins = Math.floor(seconds / 60);
+
+    const hrs = Math.floor(seconds / 3600);
+    const mins = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
+
+    if (hrs > 0) {
+      return `${hrs}:${mins.toString().padStart(2, "0")}:${secs
+        .toString()
+        .padStart(2, "0")}`;
+    }
+
     return `${mins}:${secs.toString().padStart(2, "0")}`;
   };
-  const getTimeRemaining = () => {
+
+  const getTimeDisplay = () => {
     const timeElapsedInSeconds = Math.floor(timeElapsed / 1000);
+
+    if (selectedDuration === 0) {
+      return timeElapsedInSeconds;
+    }
+
     const remaining = selectedDuration - timeElapsedInSeconds;
     return Math.max(0, remaining);
   };
+
   return (
     <div className="text-[#C68C22] text-[32px] font-medium m-[0em_0.25em] mt-3">
-      {selectedMode === "quotes" || selectedMode === "words" ? (
+      {selectedMode === "quotes" ? (
         <div>
-          {wordsTyped}/{totalWords}
+          {wordsTyped}/{totalQuoteWords}
+        </div>
+      ) : selectedMode === "words" ? (
+        <div>
+          {selectedWordCount === 0 ? (
+            <>{wordsTyped}</>
+          ) : (
+            <>
+              {wordsTyped}/{selectedWordCount}
+            </>
+          )}
         </div>
       ) : (
-        <div>{formatTime(getTimeRemaining())}</div>
+        <div>{formatTime(getTimeDisplay())}</div>
       )}
     </div>
   );
 };
+
 export default TestStatus;
