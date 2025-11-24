a51ce41ddc704c95363c4619d26aa536ab178960
uint82 <abrorhilmi6@gmail.com>
2025-11-24 22:20:40 +0700

    changes: footer component text color become dark-gray

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Mail, LifeBuoy, FileText, Shield } from "lucide-react";
 
 const Footer = ({ isTyping, onOpenContact, onOpenSupport }) => {
   const currentYear = new Date().getFullYear();
@@ -9,15 +10,15 @@ const Footer = ({ isTyping, onOpenContact, onOpenSupport }) => {
         }`}
     >
       <div className="full-width absolute -top-10 left-1/2 transform -translate-x-1/2 text-[#d5c4a1] text-xs sm:text-sm">
-        <span>
-          <span className="bg-[#3c3836] text-[#ebdbb2] px-2 py-0.5 shadow-sm mx-0.5">
-            Tab
+        <span className="full-width text-[#665c54] flex items-center gap-2 whitespace-nowrap">
+          <span className="bg-[#665c54] rounded text-black/60 px-2 py-0.1 shadow-sm mx-0.5">
+            tab
           </span>
           +
-          <span className="bg-[#3c3836] text-[#ebdbb2] px-2 py-0.5 shadow-sm mx-0.5">
+          <span className="bg-[#665c54] rounded text-black/60 px-2 py-0.1 shadow-sm mx-0.5">
             Enter
           </span>
-          = restart test
+          -<span className="text-[#665c54]">restart test</span>
         </span>
       </div>
 
@@ -25,28 +26,38 @@ const Footer = ({ isTyping, onOpenContact, onOpenSupport }) => {
         <div className="flex space-x-6 mb-3 sm:mb-0">
           <button
             onClick={onOpenContact}
-            className="hover:text-[#fe8019] cursor-pointer transition"
+            className="hover:text-white text-[#665c54] cursor-pointer transition flex items-center gap-1"
           >
-            Contact
+            <Mail size={16} />
+            contact
           </button>
 
           <button
             onClick={onOpenSupport}
-            className="hover:text-[#fe8019] cursor-pointer transition"
+            className="hover:text-white text-[#665c54] cursor-pointer transition flex items-center gap-1"
           >
-            Support
+            <LifeBuoy size={16} />
+            support
           </button>
 
-          <Link to="/terms" className="hover:text-[#fe8019] transition">
-            Terms
+          <Link
+            to="/terms"
+            className="hover:text-white text-[#665c54] transition flex items-center gap-1"
+          >
+            <FileText size={16} />
+            terms
           </Link>
 
-          <Link to="/privacy" className="hover:text-[#fe8019] transition">
-            Privacy
+          <Link
+            to="/privacy"
+            className="hover:text-white text-[#665c54] transition flex items-center gap-1"
+          >
+            <Shield size={16} />
+            privacy
           </Link>
         </div>
 
-        <p className="text-[#a89984]">
+        <p className="text-[#665c54]">
           {currentYear} inspired by{" "}
           <a
             href="https://monkeytype.com"
